0104990a3df3f1050d0170c1ab4a7fc4e5aab3d1
nbindra <nbindra@live.com>
2025-01-26 18:22:23 -0500

    most of the tf needed to get this up

---
+++ infra/main.tf
@@ -0,0 +1,103 @@
+### providers ###
+provider "azurerm" {
+  features {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+### rgs ###
+
+resource "azurerm_resource_group" "k8s_rg" {
+  name     = "${var.prefix}_k8s_rg"
+  location = var.location
+}
+
+### Networking ###
+
+resource "azurerm_virtual_network" "k8s_vnet" {
+  name                = "${var.prefix}_k8s_vnet"
+  location            = azurerm_resource_group.k8s_rg.location
+  resource_group_name = azurerm_resource_group.k8s_rg.name
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_public_ip" "k8s_fwpublic_ip" {
+  name                = "${var.prefix}-fw-public-ip"
+  location            = azurerm_resource_group.k8s_rg.location
+  resource_group_name = azurerm_resource_group.k8s_rg.name
+  allocation_method   = "Static"
+
+  tags = {
+    environment = var.prefix
+  }
+}
+
+resource "azurerm_subnet" "k8s_subnet" {
+  name                 = "k8s_subnet"
+  virtual_network_name = azurerm_virtual_network.k8s_vnet.name
+  resource_group_name  = azurerm_resource_group.k8s_rg.name
+  address_prefixes     = ["10.1.0.0/22"]
+}
+
+resource "azurerm_route_table" "k8s_rt" {
+  name                          = "${var.prefix}_k8s_rt"
+  location                      = azurerm_resource_group.k8s_rg.location
+  resource_group_name           = azurerm_resource_group.k8s_rg.name
+  bgp_route_propagation_enabled = true
+
+  route {
+    name                   = "${var.prefix}fwrn"
+    address_prefix         = "0.0.0.0/0"
+    next_hop_type          = "VirtualAppliance"
+    next_hop_in_ip_address = var.fwprivate_ip
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "k8s_rt_subnet_rel" {
+  subnet_id      = azurerm_subnet.k8s_subnet.id
+  route_table_id = azurerm_route_table.k8s_rt.id
+}
+
+resource "azurerm_network_security_rule" "allow_https" {
+  name                        = "allow-https"
+  priority                    = 127
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.k8s_rg.name
+  network_security_group_name = azurerm_network_security_group.k8s_nsg.name
+}
+
+### k8s ###
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "${var.prefix}_aks_cluster"
+  location            = azurerm_resource_group.k8s_rg.location
+  resource_group_name = azurerm_resource_group.k8s_rg.name
+  dns_prefix          = "${var.prefix}_k8s"
+
+  default_node_pool {
+    name           = "system"
+    node_count     = 1
+    vm_size        = "Standard_DS3_v2"
+    vnet_subnet_id = azurerm_subnet.k8s_subnet.id
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+    outbound_type     = "userDefinedRouting"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
---
+++ infra/vars.tf
@@ -0,0 +1,18 @@
+variable "prefix" {
+  description = "A prefix used for all resources in this example"
+  default     = "sbx"
+}
+
+variable "location" {
+  description = "The region used for this sbx"
+  default     = "east-us"
+}
+
+variable "fwprivate_ip" {
+  description = "The private IP address for internal traffic routing."
+  default     = "10.1.1.5" # This needs to match the 
+}
+
+variable "fwpublic_ip" {
+  description = "The public IP address for external traffic routing."
+}
